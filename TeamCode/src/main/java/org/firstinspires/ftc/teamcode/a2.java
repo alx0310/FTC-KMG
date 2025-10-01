b2f15bcfa059fc4ef997c4ac0d10da872007d367
KMG <3angelo09@gmail.com>
2025-10-02 00:50:28 +0900

    Std

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a2.java
@@ -1,16 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "a2")
-public class a2 extends LinearOpMode {
-
-    @Override
-    public void runOpMode() {
+@TeleOp(name = "a2")public class a2 extends LinearOpMode {
+    DcMotor mo;
+    boolean f = false;
+    boolean t = false;
+    @Override public void runOpMode() {
         waitForStart();
         if (opModeIsActive()) {
+            mo = hardwareMap.get(DcMotor.class, "0");
+
             // Pre-run
             while (opModeIsActive()) {
+                if(!gamepad1.x){
+                    f = false;
+                }
+                else if(!f){
+                    f = true;
+                    t = !t;
+
+                    mo.setPower((t) ? 0.5:0.0);
+                }
                 // OpMode loop
             }
         }
